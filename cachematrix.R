5e2cec9351a91d0b9c1d0e88a025cfa10d2ed852
jiyer90 <jahnvib97@gmail.com>
2022-08-07 14:15:23 +0530

    Program to solve the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I have set x as the matrix and z as the null. 
+##Then I get the respective matrix and null.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+z<-NULL
+set<-function(y){
+  x<<-y
+  z<<-NULL
+}
+get<-function()x
+setinverse<-function(inverse) z<<-inverse
+getinverse<-function()z
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+#this function calculates the inverse of the matrix. 
+##If it is computed already, the function will get it from the cache. 
+##Otherwise, it will compute the inverse and set the cache value equal to it using setinverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       z<-x$getinverse()
+       if(!is.null(z)){
+         message("getting cached inverse")
+         return(z)
+       }
+       data<-x$get()
+       z<-solve(data,...)
+       x$setinverse(z)
+       z
 }
